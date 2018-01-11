20ef0293f9320ab7ddb74bd36e53ab9d42ea0db5
KariAnn Yoshida <yoshidakariann@gmail.com>
2018-01-10 16:05:20 -0800

    fixed the commenting error

--- src/List.js
+++ src/List.js
@@ -4,7 +4,7 @@ const List = ({ items, deleteItem }) => (
   <ul>
     {
       items.map((item, index) => <li key={index}>{item} <button key={item} onClick={(e) => deleteItem(e, index)}>Remove</button> </li>)
-    {/* the onClick has a function and we are passing back the index to the App.js page. the onClick is taking in an event, and it is running the deleteItem function while taking in the event (e) and the index which is generated when you generate the li*/}
+    /* the onClick has a function and we are passing back the index to the App.js page. the onClick is taking in an event, and it is running the deleteItem function while taking in the event (e) and the index which is generated when you generate the li*/
     }
   </ul>
 );
